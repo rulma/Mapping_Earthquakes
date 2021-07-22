a850720048b2b334f51d778617548ed3f52cba4b
Mateo Rul <mateoarul@gmail.com>
2021-07-22 16:26:47 -0500

    simple map

---
+++ Simple_Map/logic.js
@@ -0,0 +1,25 @@
+// CHECK 
+
+console.log("working");
+
+// Create Map Object
+
+let map = L.map("mapid", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
+
+let streets  = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+streets.addTo(map);
+
+
