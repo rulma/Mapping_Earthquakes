10ac86510b0212a7e69093b71572df18c399e57e
Mateo Rul <mateoarul@gmail.com>
2021-07-26 15:30:19 -0500

    Mapped airline route

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,77 @@
+// CHECK 
+
+console.log("working");
+
+// Create Map Object
+// An array containing each city's location, state, and population.
+let cities = [{
+  location: [40.7128, -74.0059],
+  city: "New York City",
+  state: "NY",
+  population: 8398748
+},
+{
+  location: [41.8781, -87.6298],
+  city: "Chicago",
+  state: "IL",
+  population: 2705994
+},
+{
+  location: [29.7604, -95.3698],
+  city: "Houston",
+  state: "TX",
+  population: 2325502
+},
+{
+  location: [34.0522, -118.2437],
+  city: "Los Angeles",
+  state: "CA",
+  population: 3990456
+},
+{
+  location: [33.4484, -112.0740],
+  city: "Phoenix",
+  state: "AZ",
+  population: 1660272
+}
+];
+
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+  console.log(city)
+ });
+
+ let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+
+let streets  = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+streets.addTo(map);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.circle([34.0522, -118.2437], { radius: 3000, color: "black", fillColor: "#ffffa1"}).addTo(map);
+
+cities.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+console.log(line)
+
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
