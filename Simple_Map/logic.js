35917b73889611d34d908bd4f48457fd2b2a02bd
Mateo Rul <mateoarul@gmail.com>
2021-07-26 12:39:44 -0500

    Created a Simple Map

--- Simple_Map/logic.js
+++ Simple_Map/logic.js
@@ -14,9 +14,6 @@ let map = L.map("mapid", {
 let streets  = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
     accessToken: API_KEY
 });
 
